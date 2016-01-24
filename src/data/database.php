c1d908e6ee2841c7cfbe46b681c094244aeb567b
Russell Bishop <russell@wearelighthouse.com>
2016-01-24 22:53:59 +0000

    Database switching

--- src/data/database.php
+++ src/data/database.php
@@ -1,6 +1,10 @@
 <?php
 
-	$database = new medoo([
+	$url = 'http://' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
+	
+	if (strpos($url,'localhost') !== false) {
+		
+		$database = new medoo([
 	    'database_type' => 'mysql',
 	    'database_name' => 'bandit',
 	    'server' => '127.0.0.1',
@@ -8,5 +12,22 @@
 	    'password' => 'login',
 	    'charset' => 'utf8'
 	]);
+			
+	}
+	
+	else  {
+		
+		$database = new medoo([
+		    'database_type' => 'mysql',
+		    'database_name' => 'russ_bandit',
+		    'server' => 'localhost',
+			'username' => 'russ_coin',
+			'password' => 'insdj08921jd210',
+			'charset' => 'utf8',
+		]);
+		
+	}
+
+	
 	
 ?>
