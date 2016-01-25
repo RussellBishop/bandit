14d247e50cca4e265c4d82fd78f56acd6cb9881c
Russell Bishop <russell@wearelighthouse.com>
2016-01-25 00:51:39 +0000

    Split Leaderboard into 'Waiting'

--- leaderboard.php
+++ leaderboard.php
@@ -4,8 +4,6 @@
 	
 	require_once('src/config.php');
 	
-	require_once($data.'players.php');
-	
 	require_once($functions.'functions.php');
 	
 	require($template.'header.php');
@@ -35,13 +33,21 @@
 	
 	foreach ($leaderboardPlayers as $player) {
 
-		$playerStats = playerStats($player['id']);
-
-		$matchesPlayed = matchesPlayed($player['id']);
-
-		print_r($matchesPlayed);
+		$matchesPlayed = $database->count('matches',
+			[
+				'AND' => [
+					'OR' => [
+						'winner' => $player['id'],
+						'loser' => $player['id']
+					],
+					'accepted' => '1',
+				],
+			]
+		);
 
 		if ($matchesPlayed > 0) {
+
+			$playerStats = playerStats($player['id']);
 
 			$lastGame = $database->select('matches',
 				[
@@ -102,7 +108,7 @@
 				
 				';
 			
-			echo '</div></li>';
+			echo '</a></li>';
 
 		}
 		
@@ -111,7 +117,112 @@
 	?>
 	
 	</ol>
+
+</article>
+
+<article class="block leaderboard">
+
+	<h1 class="h1">Waiting&hellip;</h1>
+
+	<ol>
+	
+	<?php
+	
+	$leaderboardPlayers = $database->select('players',
+	
+		[
+			'id', 'name', 'photo', 'rating'
+		],
 		
+		[
+			"ORDER" => ['rating DESC']
+		]
+		
+	);
+	
+	foreach ($leaderboardPlayers as $player) {
+
+		$matchesPlayed = $database->count('matches',
+			[
+				'AND' => [
+					'OR' => [
+						'winner' => $player['id'],
+						'loser' => $player['id']
+					],
+					'accepted' => '1',
+				],
+			]
+		);
+
+		if ($matchesPlayed == 0) {
+
+			$playerStats = playerStats($player['id']);
+
+			$lastGame = $database->select('matches',
+				[
+					'datetime', 'winner', 'loser'
+				],
+				
+				[
+					'AND' => [
+						'OR' => [
+							'winner' => $player['id'],
+							'loser' => $player['id']
+						],
+						'accepted' => '1',
+					],
+					
+					"ORDER" => "datetime DESC",
+					"LIMIT" => 1
+				]
+			);
+
+			if ($lastGame[0]['winner'] == $player['id']) {
+				$lastGameResult = 'is--winner';
+			}
+			elseif ($lastGame[0]['loser'] == $player['id']) {
+				$lastGameResult = 'is--loser';
+			}
+
+			if ($player['id'] == $you['id']) {
+				$isYou = 'is--you';
+			}
+			else {
+				$isYou = '';
+			}
+			
+			echo '<li><a href="player.php?player=' .$player['id']. '" class="g3 slate is--pending is--position ' .$lastGameResult. ' is--level' . $playerStats['levelId']. ' ' .$isYou. '">
+			
+				<div class="base"></div>
+				
+				<div class="col1">';
+				
+				playerPhoto($player['id']);
+				
+				echo '
+				</div>
+				
+				<header class="col2 player-info">
+					<h1>' .$player['name']. '</h1>
+					<h2 class="player-level">' .$playerStats['level']. '</h2>
+				</header>
+				
+				<aside class="col3 player-rating">
+					<h3>' .$playerStats['rating']. '</h3>
+				</aside>
+				
+				';
+			
+			echo '</a></li>';
+
+		}
+		
+	}
+	
+	?>
+	
+	</ol>
+
 </article>
 
 <?php
