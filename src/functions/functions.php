89868c9482ea995a3596cd2437968ee8d6397a03
Russell Bishop <russell@wearelighthouse.com>
2016-02-02 00:38:35 +0000

    More functions

--- src/functions/functions.php
+++ src/functions/functions.php
@@ -1,13 +1,16 @@
 <?php
 
 	require_once($functions.'timeSince.php');
-	require_once($functions.'showRating.php');
 	require_once($functions.'winLossRatio.php');
+
+	require_once($functions.'matchesPlayed.php');
+	require_once($functions.'playerPosition.php');
 	require_once($functions.'playerStats.php');
 	require_once($functions.'playerPhoto.php');
 	require_once($functions.'playerPhotoInline.php');
 	require_once($functions.'acronym.php');
-	require_once($functions.'matchesPlayed.php');
-	require_once($functions.'playerPosition.php');
+	
+	require_once($functions.'gamesBeforeToday.php');
+	require_once($functions.'selectLastGameBeforeToday.php');
 	
 ?>
