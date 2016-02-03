0b3ea0abe19a3374d5b841f5e9a968424d9e51b6
Russell Bishop <russell@wearelighthouse.com>
2016-02-03 00:07:06 +0000

    Menu popup

--- src/template/navigation.php
+++ src/template/navigation.php
@@ -22,16 +22,14 @@
 
 <nav class="menu">
 
-	<label class="menu__toggle" for="menu">
+	<label class="menu__toggle" data-toggles="menu">
 		<div class="player-photo-wrap is--level<?=$playerStats['levelId']?>"><?php playerPhoto($you['id']); ?></div>
 		<figure class="menu__icon"><?php if ($notifications>0) { echo '<span class="new">'.$notifications.'</span>'; } ?></span><?php echo file_get_contents('src/img/menu.svg'); ?></figure>
 	</label>
 
-	<input class="toggle" name="menu" id="menu" type="checkbox" />
+	<div class="fullscreen is--menu" data-is="menu">
 
-	<div class="fullscreen is--menu toggled">
-
-		<label for="menu" class="close">X</label>
+		<label class="close" data-toggles="menu">X</label>
 
 		<ul>
 			<li class="<?php if ($view == 'profile') { echo 'is--open'; } ?>"><a href="/">My Profile</a></li>
